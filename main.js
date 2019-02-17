2979779ae089f30badfb7136933767990e792602
Samuel Stenton <sthstenton@gmail.com>
2019-02-17 19:08:01 +0000

    weew

--- main.js
+++ main.js
@@ -8,16 +8,18 @@ const createRepo = async (event, context, callback) => {
   body = JSON.parse(event.body);
   const token = await getInstallationAccessToken();
 
+  console.log(token);
   axios({
     method: 'post',
     url: `https://api.github.com/orgs/${org}/repos`,
     data: {
       "name": body.name,
       "description": body.description,
+      "homepage": body.homepage,
       "private": false,
-      "has_issues": false,
-      "has_projects": false,
-      "has_wiki": false
+      "has_issues": body.has_issues,
+      "has_projects": body.has_rojects,
+      "has_wiki": body.has_wiki
     },
     headers: {
       "Authorization": `Bearer ${token}`,
